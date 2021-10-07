fb863bf8d168e2647fe9e91f96eef2c8c7852eaa
Alvaro <alv_saul@hotmail.com>
2021-10-07 17:24:26 -0500

    perfil de usuario

--- src/app/main/center/perfil-usuario/perfil-usuario.component.ts
+++ src/app/main/center/perfil-usuario/perfil-usuario.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { PerfilUsuarioService } from './perfil-usuario.service';
+import { User } from '../../../auth/models/user';
+import { CoreMenuService } from '../../../../@core/components/core-menu/core-menu.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { InformacionBasica } from '../../personas/models/persona';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-perfil-usuario',
   templateUrl: './perfil-usuario.component.html',
-  styleUrls: ['./perfil-usuario.component.scss']
+  styleUrls: ['./perfil-usuario.component.scss'],
+  providers: [DatePipe]
 })
 export class PerfilUsuarioComponent implements OnInit {
+  public tab;
+  public usuario: User;
+  public coreConfig: any;
+  public personaForm: FormGroup;
+  public informacionBasica: InformacionBasica;
+  public persona;
+  public imagen;
+  // Private
+  private _unsubscribeAll: Subject<any>;
+  constructor(
+    private _perfilUsuarioService: PerfilUsuarioService,
+    private _coreMenuService: CoreMenuService,
+    private _formBuilder: FormBuilder,
+    private datePipe: DatePipe,
+  ) {
+    this._unsubscribeAll = new Subject();
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    this.personaForm = this._formBuilder.group({
+      created_at: ['',],
+      identificacion: ['',],
+      nombres: ['',],
+      apellidos: ['',],
+      genero: ['',],
+      fechaNacimiento: ['string',],
+      edad: ['',],
+      whatsapp: ['',],
+      ciudad: ['',],
+      email: ['',],
+      emailAdicional: ['',],
+      facebook: ['',],
+      instagram: ['',],
+      twitter: ['',],
+      tiktok: ['',],
+      youtube: ['',],
+    });
+    this.usuario = this._coreMenuService.currentUser;
+    this._perfilUsuarioService.obtenerInformacion(this.usuario.id).subscribe(info => {
+      this.imagen = info.imagen;
+      info.created_at = this.transformarFecha(info.created_at);
+      info.fechaNacimiento = this.transformarFecha(info.fechaNacimiento);
+      this.personaForm.patchValue(
+        info,
+      );
+    });
+  }
+  transformarFecha(fecha) {
+    let nuevaFecha = this.datePipe.transform(fecha, 'yyyy-MM-dd');
+    return nuevaFecha;
+  }
+  guardarInformacion() {
+    this.informacionBasica = this.personaForm.value;
+    console.log(this.personaForm.value);
+  }
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
 }
--- src/app/main/personas/models/persona.ts
+++ src/app/main/personas/models/persona.ts
@@ -6,5 +6,18 @@ export class CompletarPerfil {
     fechaNacimiento: string;
     edad: number;
     whatsapp: string;
-    user_id:string;
+    user_id: string;
+}
+export class InformacionBasica {
+    genero: string;
+    fecha: string;
+    edad: number;
+    ciudad: string;
+    emailAdicional: string;
+    whatsapp: string;
+    facebook: string;
+    instagram: string;
+    twitter: string;
+    tiktok: string;
+    youtube: string;
 }
