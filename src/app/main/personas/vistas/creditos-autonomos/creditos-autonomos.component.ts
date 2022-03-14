20721703c5e432ad71f2e8ed36f1169193466c70
Jimmy Yar <jimmy1817@hotmail.com>
2022-03-14 10:52:47 -0500

    Correccion de la creacion del credito de personas

--- src/app/main/personas/vistas/creditos-autonomos/creditos-autonomos.component.ts
+++ src/app/main/personas/vistas/creditos-autonomos/creditos-autonomos.component.ts
@@ -191,7 +191,6 @@ export class CreditosAutonomosComponent implements OnInit {
       this.solicitarCredito.nombres = this.usuario.persona.nombres;
       this.solicitarCredito.apellidos = this.usuario.persona.apellidos;
       this.solicitarCredito.numeroIdentificacion = this.usuario.persona.identificacion;
-      console.log(this.solicitarCredito)
       this._creditosAutonomosService.crearCredito(this.solicitarCredito).subscribe((info) => {
         this.proceso = value;
       });
