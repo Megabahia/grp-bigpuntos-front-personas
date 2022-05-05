38b400e99997b8363d1473b31f3f7ad529089a99
Jimmy Yar <jimmy1817@hotmail.com>
2022-05-05 12:03:45 -0500

    [Jimmy] Cambiar modal de confirmar el usuario.

--- src/app/main/personas/vistas/completar-perfil/completar-perfil.service.ts
+++ src/app/main/personas/vistas/completar-perfil/completar-perfil.service.ts
@@ -1,30 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CompletarPerfilService {
 
-  constructor(private _httpClient: HttpClient) { }
+    constructor(private _httpClient: HttpClient) {
+    }
 
 
-  subirImagenRegistro(id, imagen) {
-    return this._httpClient.post<any>(`${environment.apiUrl}/personas/personas/update/imagen/${id}`, imagen);
-  }
+    subirImagenRegistro(id, imagen) {
+        return this._httpClient.post<any>(`${environment.apiUrl}/personas/personas/update/imagen/${id}`, imagen);
+    }
 
-  guardarInformacion(datos) {
-    return this._httpClient.post<any>(`${environment.apiUrl}/personas/personas/update/${datos.user_id}`, datos);
-  }
+    guardarInformacion(datos) {
+        return this._httpClient.post<any>(`${environment.apiUrl}/personas/personas/update/${datos.user_id}`, datos);
+    }
 
-  obtenerInformacion(id) {
-    return this._httpClient.get<any>(`${environment.apiUrl}/personas/personas/listOne/${id}`, );
-  }
+    obtenerInformacion(id) {
+        return this._httpClient.get<any>(`${environment.apiUrl}/personas/personas/listOne/${id}`,);
+    }
 
-  validarWhatsapp(datos){
-    return this._httpClient.post<any>(`${environment.apiUrl}/personas/personas/validarCodigo/`, datos );
+    validarWhatsapp(datos) {
+        return this._httpClient.post<any>(`${environment.apiUrl}/personas/personas/validarCodigo/`, datos);
 
-  }
+    }
 
 }
