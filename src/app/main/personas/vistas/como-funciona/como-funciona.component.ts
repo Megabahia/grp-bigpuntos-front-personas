8efbe11c98fe57646a2fe419387beff28b959844
Jimmy Yar <jimmy1817@hotmail.com>
2022-06-10 09:49:33 -0500

    [Jimmy] Modificar pantallas, que es, como funciona, donde canjear, reclamos y requerimientos

--- src/app/main/personas/vistas/como-funciona/como-funciona.component.ts
+++ src/app/main/personas/vistas/como-funciona/como-funciona.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ParametrizacionesService} from '../../servicios/parametrizaciones.service';
 
 @Component({
-  selector: 'app-como-funciona',
-  templateUrl: './como-funciona.component.html',
-  styleUrls: ['./como-funciona.component.scss']
+    selector: 'app-como-funciona',
+    templateUrl: './como-funciona.component.html',
+    styleUrls: ['./como-funciona.component.scss']
 })
 export class ComoFuncionaComponent implements OnInit {
+    public imagenes = [];
+    private _unsubscribeAll: Subject<any>;
 
-  constructor() { }
+    constructor(
+        private _paramService: ParametrizacionesService,
+        private sanitizer: DomSanitizer
+    ) {
+        this._unsubscribeAll = new Subject();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    // tslint:disable-next-line:use-life-cycle-interface
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
+
+    obtenerURL(i) {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(this.imagenes[i].url);
+    }
 
 }
