7bafb1417531fe7c1ee6148f37c9d0ee70eec03f
Abraham Vallejos <abraham-rat@outlook.es>
2022-08-22 11:46:01 -0500

    Implemented [Abraham Vallejos]: id App FB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,57 +1,57 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import "hammerjs";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { TranslateModule } from "@ngx-translate/core";
-import { ToastrModule } from "ngx-toastr"; // For auth after login toast
+import 'hammerjs';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr'; // For auth after login toast
 
-import { CoreModule } from "@core/core.module";
-import { CoreCommonModule } from "@core/common.module";
-import { CoreSidebarModule, CoreThemeCustomizerModule } from "@core/components";
+import { CoreModule } from '@core/core.module';
+import { CoreCommonModule } from '@core/common.module';
+import { CoreSidebarModule, CoreThemeCustomizerModule } from '@core/components';
 
-import { coreConfig } from "app/app-config";
-import { NgxCaptchaModule } from "ngx-captcha";
+import { coreConfig } from 'app/app-config';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
-import { AppComponent } from "app/app.component";
-import { LayoutModule } from "app/layout/layout.module";
-import { SampleModule } from "app/main/sample/sample.module";
-import { AuthGuard } from "./auth/helpers/auth.guards";
-import { JwtInterceptor } from "./auth/helpers/jwt.interceptor";
-import { ErrorInterceptor } from "./auth/helpers/error.interceptor";
+import { AppComponent } from 'app/app.component';
+import { LayoutModule } from 'app/layout/layout.module';
+import { SampleModule } from 'app/main/sample/sample.module';
+import { AuthGuard } from './auth/helpers/auth.guards';
+import { JwtInterceptor } from './auth/helpers/jwt.interceptor';
+import { ErrorInterceptor } from './auth/helpers/error.interceptor';
 import {
   FacebookLoginProvider,
   SocialAuthServiceConfig,
   SocialLoginModule,
-} from "angularx-social-login";
+} from 'angularx-social-login';
 
 const appRoutes: Routes = [
   {
-    path: "",
-    redirectTo: "personas",
-    pathMatch: "full",
+    path: '',
+    redirectTo: 'personas',
+    pathMatch: 'full',
   },
   {
-    path: "pages",
+    path: 'pages',
     loadChildren: () =>
-      import("./main/pages/pages.module").then((m) => m.PagesModule),
+      import('./main/pages/pages.module').then((m) => m.PagesModule),
   },
   {
-    path: "grp",
+    path: 'grp',
     loadChildren: () =>
-      import("./main/center/center.module").then((m) => m.CenterModule),
+      import('./main/center/center.module').then((m) => m.CenterModule),
   },
   {
-    path: "personas",
+    path: 'personas',
     loadChildren: () =>
-      import("./main/personas/personas.module").then((m) => m.PersonasModule),
+      import('./main/personas/personas.module').then((m) => m.PersonasModule),
   },
   {
-    path: "**",
-    redirectTo: "/pages/miscellaneous/error", //Error 404 - Page not found
+    path: '**',
+    redirectTo: '/pages/miscellaneous/error', // Error 404 - Page not found
   },
 ];
 
@@ -62,13 +62,13 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes, {
-      scrollPositionRestoration: "enabled", // Add options right here
-      relativeLinkResolution: "legacy",
+      scrollPositionRestoration: 'enabled', // Add options right here
+      relativeLinkResolution: 'legacy',
       useHash: true,
     }),
     TranslateModule.forRoot(),
 
-    //NgBootstrap
+    // NgBootstrap
     NgbModule,
     ToastrModule.forRoot(),
 
@@ -86,13 +86,13 @@ const appRoutes: Routes = [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     {
-      provide: "SocialAuthServiceConfig",
+      provide: 'SocialAuthServiceConfig',
       useValue: {
         autoLogin: false,
         providers: [
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider("489448599659628"),
+            provider: new FacebookLoginProvider('1056794005208514'),
           },
         ],
       } as SocialAuthServiceConfig,
