9fa2df07a3379a36849dd09b6c3aaca9965fa0ff
Abraham Vallejos <aronpc1997@gmail.com>
2022-10-18 14:45:50 -0500

    [Abraham Vallejos]: Add views pre-approved in app

--- src/app/main/pages/approved-end-consumer/approved-end-consumer.component.ts
+++ src/app/main/pages/approved-end-consumer/approved-end-consumer.component.ts
@@ -7,91 +7,91 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import Decimal from 'decimal.js';
 
 @Component({
-  selector: 'app-approved-end-consumer',
-  templateUrl: './approved-end-consumer.component.html',
-  styleUrls: ['./approved-end-consumer.component.scss']
+    selector: 'app-approved-end-consumer',
+    templateUrl: './approved-end-consumer.component.html',
+    styleUrls: ['./approved-end-consumer.component.scss']
 })
-export class ApprovedEndConsumerComponent  implements OnInit {
-  public envioForm: FormGroup;
-  public submittedSimulador = false;
-  public pathSent;
-  @ViewChild('mensajeModal') mensajeModal;
-  public mensaje = '';
-  public monto;
-  public usuario;
-  constructor(
-      private _coreConfigService: CoreConfigService,
-      private _formBuilder: FormBuilder,
-      private _router: ActivatedRoute,
-      private _routerN: Router,
-      private _preArpovedCreditService: PreArpovedCreditService,
-      private modalService: NgbModal,
-  ) {
-    const ref = document.referrer;
-    const host = document.location.host;
-    this._router.queryParams.subscribe((params) => {
-      this.monto = params.monto;
-      this.usuario = params.nombreCompleto;
-    });
+export class ApprovedEndConsumerComponent implements OnInit {
+    public envioForm: FormGroup;
+    public submittedSimulador = false;
+    public pathSent;
+    @ViewChild('mensajeModal') mensajeModal;
+    public mensaje = '';
+    public monto;
+    public usuario;
 
-    if (localStorage.getItem('preApproved')) {
-      this._router.queryParams.subscribe((params) => {
-        this.monto = params.monto;
-        this.usuario = params.nombreCompleto;
-      });
-      localStorage.removeItem('preApproved');
-    } else {
-      this.actionContinue();
+    constructor(
+        private _coreConfigService: CoreConfigService,
+        private _formBuilder: FormBuilder,
+        private _router: ActivatedRoute,
+        private _routerN: Router,
+        private _preArpovedCreditService: PreArpovedCreditService,
+        private modalService: NgbModal,
+    ) {
+        const ref = document.referrer;
+        const host = document.location.host;
+        this._router.queryParams.subscribe((params) => {
+            this.monto = params.monto;
+            this.usuario = params.nombreCompleto;
+        });
+
+        if (localStorage.getItem('preApproved')) {
+            this._router.queryParams.subscribe((params) => {
+                this.monto = params.monto;
+                this.usuario = params.nombreCompleto;
+            });
+            localStorage.removeItem('preApproved');
+        } else {
+            this.actionContinue();
+        }
+
+        this._coreConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: true,
+                },
+                footer: {
+                    hidden: true,
+                },
+                menu: {
+                    hidden: true,
+                },
+                customizer: false,
+                enableLocalStorage: false,
+            },
+        };
     }
 
-    this._coreConfigService.config = {
-      layout: {
-        navbar: {
-          hidden: true,
-        },
-        footer: {
-          hidden: true,
-        },
-        menu: {
-          hidden: true,
-        },
-        customizer: false,
-        enableLocalStorage: false,
-      },
-    };
-  }
+    get getsimuladorForm() {
+        return this.envioForm.controls;
+    }
 
-  get getsimuladorForm() {
-    return this.envioForm.controls;
-  }
-
-  cerrarModal() {
-    this.modalService.dismissAll();
-  }
+    cerrarModal() {
+        this.modalService.dismissAll();
+    }
 
 
+    abrirModal(modal) {
+        this.modalService.open(modal);
+    }
 
-  abrirModal(modal) {
-    this.modalService.open(modal);
-  }
+    ngOnInit(): void {
+        this.envioForm = this._formBuilder.group({
+            code: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern('^[0-9]*$'),
+                    Validators.min(1),
+                ],
+            ],
+        });
+    }
 
-  ngOnInit(): void {
-    this.envioForm = this._formBuilder.group({
-      code:  [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('^[0-9]*$'),
-          Validators.min(1),
-        ],
-      ],
-    });
-  }
-
-  actionContinue() {
-    this._routerN.navigate([
-      `/`,
-    ]);
-    localStorage.setItem('preApproved', 'true');
-  }
+    actionContinue() {
+        this._routerN.navigate([
+            `/`,
+        ]);
+        localStorage.setItem('preApproved', 'true');
+    }
 }
